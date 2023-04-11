9f19f48147852300b11e7837b8008d9d2d056b52
Violeta Solorio <108785302+violetxs16@users.noreply.github.com>
2023-04-10 19:27:30 -0700

    Update Violeta_Solorio_project4_script1.php

--- Violeta_Solorio_project4_script1.php
+++ Violeta_Solorio_project4_script1.php
@@ -2,30 +2,29 @@
 <html lang = "en">
 <head>
 	<meta charset = "utf-8">
-	<title> Connect to MySQL</title>
+	<title> Favorite Game Database creation</title>
 </head>
 <body>
-
 <?php
 /*
 The following Script will create the following the table: data
 If the tables could not be created a error message will occur
-*/
+*
 	//project4_database was created through phpMyAdmin
 	if($database = @mysqli_connect('localhost','username','thepassword', 'project4_database')){//Establishes connection to database & assigns it to variable $database
 		print "Connected!";
-		$query = 'CREATE TABLE data(id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, age INT NOT NULL, game TEXT NOT NULL )';//Creates table for fruits
-		if(@mysqli_query($database, $query)){//Checks if fruits table is created
+		$query = 'CREATE TABLE data(id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, age INT NOT NULL, game TEXT NOT NULL )';
+		if(@mysqli_query($database, $query)){
 			print "<h3>Table was sucessfully Created</h3>";
 			
-		}else{//Fruits table was not created
-			print "<h3> Could not create fruits table because: ". mysqli_error($database);	
+		}else{
+			print "<h3> Could not create favorite game table because: ". mysqli_error($database);	
 		}
 		mysqli_close($database);//Closes the database
 	}	
 	
 	else{//Could not establish connection to the database
-		print"was not able to connect because of". mysqli_connect_error();
+		print"Could not connect to database because: ". mysqli_connect_error();
 	}
 
 ?>
